73828a7235b961055227350dce3b9c80847d3de5
nayaksneha07482 <nayaksneha07482@gmail.com>
2023-03-08 19:33:10 +0530

    header sticky js

--- Foodie/assets/js/script.js
+++ Foodie/assets/js/script.js
@@ -19,3 +19,20 @@ for (let i = 0; i < navbarLinks.length; i++) {
     menuToggleBtn.classList.toggle("active");
   });
 }
+
+/**
+ * header sticky & back to top
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
