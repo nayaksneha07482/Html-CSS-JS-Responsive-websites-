d299a792967c73928edf87f99e8aaef29d57590e
nayaksneha07482 <nayaksneha07482@gmail.com>
2023-03-08 19:48:32 +0530

    move cycle on

--- Foodie/assets/js/script.js
+++ Foodie/assets/js/script.js
@@ -37,3 +37,31 @@ window.addEventListener("scroll", function () {
   }
 });
 
+
+/**
+ * move cycle on scroll
+ */
+
+const deliveryBoy = document.querySelector("[data-delivery-boy]");
+
+let deliveryBoyMove = -80;
+let lastScrollPos = 0;
+
+window.addEventListener("scroll", function () {
+
+  let deliveryBoyTopPos = deliveryBoy.getBoundingClientRect().top;
+
+  if (deliveryBoyTopPos < 500 && deliveryBoyTopPos > -250) {
+    let activeScrollPos = window.scrollY;
+
+    if (lastScrollPos < activeScrollPos) {
+      deliveryBoyMove += 1;
+    } else {
+      deliveryBoyMove -= 1;
+    }
+
+    lastScrollPos = activeScrollPos;
+    deliveryBoy.style.transform = `translateX(${deliveryBoyMove}px)`;
+  }
+
+});
